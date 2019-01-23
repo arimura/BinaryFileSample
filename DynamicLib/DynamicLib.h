57c2106ecace1d2f84cdab1251d1899b15bb748c
Kotaro Arimura <kotaro_arimura@voyagegroup.com>
2019-01-23 18:20:05 +0900

    Add dynamic framework

---
+++ DynamicLib/DynamicLib.h
@@ -0,0 +1,18 @@
+//
+//  DynamicLib.h
+//  DynamicLib
+//
+//  Copyright © 2019年 hormiga6. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DynamicLib.
+FOUNDATION_EXPORT double DynamicLibVersionNumber;
+
+//! Project version string for DynamicLib.
+FOUNDATION_EXPORT const unsigned char DynamicLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DynamicLib/PublicHeader.h>
+
+
